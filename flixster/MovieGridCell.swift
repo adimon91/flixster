9807d08deb816c0f0cda4a903df8d5e2ea4ee678
A Dimon <51017365+adimon91@users.noreply.github.com>
2021-02-22 21:39:39 -1000

    Part 2 flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Angie Thielk on 2/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
